1d63a7c8e08a77f3a86e54c60361b9e202d913cc
Your Name <eriegian457@gmail.com>
2026-02-26 09:02:16 +0800

    library

---
+++ pages/studentlogin.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UMDC LIBRARY ACCOUNT</title>
+    <link rel="stylesheet" href="../css/studentlogin.css">
+</head>
+<body>
+    <h1 class="umdc">University of Mindanao Digos College</h1>
+</body>
+</html>
