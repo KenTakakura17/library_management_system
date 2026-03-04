3f0d61b3d11047cc451977c12f579958b9334714
Your Name <eriegian457@gmail.com>
2026-03-04 09:08:22 +0800

    lol

--- pages/studentlogin.php
+++ pages/studentlogin.php
@@ -1,12 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>UMDC LIBRARY ACCOUNT</title>
     <link rel="stylesheet" href="../css/studentlogin.css">
 </head>
-<body>
-    <h1 class="umdc">University of Mindanao Digos College</h1>
+
+<body class="container">
+    <div class="Loginmain">
+        <h1 class="umdc">University of Mindanao Digos College</h1>
+        <h2 class="library">Library Account</h2>
+    </div>
+    <div class="LoginFormContainer">
+        <form action="../php/studentlogin.php" method="POST" class="LoginForm">
+            <input type="text" name="username" placeholder="Username" required>
+            <input type="password" name="password" placeholder="Password" required>
+            <button type="submit">Login</button>
+        </form>
+    </div>
+
 </body>
+
 </html>
